3bdd91b31c1692c0d785fcfa71f28c6a8538d3cc
Nicephora <48016826+Nicephora@users.noreply.github.com>
2019-04-04 22:37:39 +0200

    Update 00_hello.rb

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -6,7 +6,7 @@ end
 
 def say_hello
 	first_name = ask_first_name.to_s #On s'assure que ce soit bien un string
-	if(first_name.size == 0) #Si la taille de la chaine est == 0 alors la chaine est vide, donc on affiche une autre phrase (pour déconner)
+	if(first_name.size == 0) 
 		puts "Et bien on dirait que tu n'as pas de prénom"
 	else
 		puts "Bonjour #{first_name} !"
